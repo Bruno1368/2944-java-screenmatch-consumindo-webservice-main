2ed25ef59063cffb0d64d43ba1d44e2f21c86fa3
Bruno <bruno.luis.999@hotmail.com>
2023-10-04 16:30:05 -0300

    Implementando a comunicação com o Web Service

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,34 @@
+package br.com.alura.screenmatch.principal;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.CookieHandler;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=1c0361f4"))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
+
+
+    }
+
+}
